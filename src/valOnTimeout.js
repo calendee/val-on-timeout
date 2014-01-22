b8d384dd916fd64fd8b5a3dd1b7d912671ec13b4
Justin Noel <github@calendee.com>
2014-01-22 07:32:02 -0600

    Documented Code
    Signed-off-by: Justin Noel <github@calendee.com>

--- src/valOnTimeout.js
+++ src/valOnTimeout.js
@@ -7,31 +7,46 @@ angular.module('calendee.valOnTimeout',[]).
             require     : 'ngModel',
             link        : function(scope, elem, attrs, ctrl) {
 
-                var timer;
-                var typingLimit = 1250;
-                var focusLimit;
-                var blurLimit;
+                var timer, typingLimit, focusLimit, blurLimit;
 
                 ctrl.$timedout = false;
 
-                if(attrs.typingLimit) typingLimit = parseInt(attrs.typingLimit, 10) || typingLimit;
+                if(attrs.typingLimit) typingLimit = parseInt(attrs.typingLimit, 10) || 1250;
                 if(attrs.focusLimit) focusLimit = parseInt(attrs.focusLimit, 10) || 5000;
                 if(attrs.blurLimit) blurLimit = parseInt(attrs.blurLimit, 10) || 500;
 
+
+                /**
+                 * Start timer for specified period.  If not cancelled before triggered, the 'timeout' property will
+                 * be added to the field.
+                 *
+                 * @param timeLimit
+                 */
                 var startTimer = function(timeLimit) {
 
                     timer = $timeout(function(){
-                        scope.$apply(function() {ctrl.$timedout = true;});
+
+                        scope.$apply( function() {
+                            ctrl.$timedout = true;
+                        });
+
                     }, timeLimit);
 
                 };
 
+                /**
+                 * Cancel any existing timer.
+                 */
                 var cancelTimer = function() {
 
                     $timeout.cancel(timer);
 
                 };
 
+                /**
+                 * Will start a timer after focus event fired for field.
+                 * Used to detect when field receives focus but typing never starts
+                 */
                 if(attrs.focusLimit) {
 
                     elem.bind('focus', function(){
@@ -48,6 +63,9 @@ angular.module('calendee.valOnTimeout',[]).
                     })
                 }
 
+                /**
+                 * Will start a timer after blur event fired for field
+                 */
                 if(attrs.blurLimit) {
 
                     elem.bind('blur', function(){
@@ -64,6 +82,9 @@ angular.module('calendee.valOnTimeout',[]).
                     })
                 }
 
+                /**
+                 * Will start a timer after each keypress to detect when typing stops
+                 */
                 elem.bind('keydown', function() {
 
                     // If the field has already timedout, don't change it.
